855b6194e40fa4a02ab88210990e7ac38a35263b
samyogadk24 <samyogdbs19@gmail.com>
2026-06-29 16:03:48 +0545

    first commit

---
+++ LearningCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.VisualBasic;
+class Program
+{
+    static void Main(string[]args)
+    {
+       // Console.WriteLine("hello !");
+
+       Info information = new Info();
+       Info.Dispay();
+    }
+}
---
+++ LearningCSharp/Question2.cs
@@ -0,0 +1,13 @@
+//Create two integer variables and print their sum.
+
+using System;
+
+public class Question2
+{
+    public void Sum()
+    {
+        int a = 10;
+        int b = 5;
+        
+    }
+}
